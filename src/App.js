58f85a1588235e10e8197c52f488f02ccf4c3a36
Antrâxx <h.julien62@gmail.com>
2022-06-10 10:31:06 +0200

    Base front

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@
 function App() {
   return (
     <div>
+      <div className="container">
 
+        <div className="d-flex flex-column">
+          <div className="panelQuestion d-flex flex-column align-items-center">
+            <h1 id="title">My Quizz</h1>
+            <h2 id="question">Question X</h2>
+          </div>
+
+          <div className="panelAnswer d-flex flex-column">
+            <button className="btn btn-lg btn-primary text-light rounded my-1">Answer x</button>
+            <button className="btn btn-lg btn-primary text-light rounded my-1">Answer x</button>
+            <button className="btn btn-lg btn-primary text-light rounded my-1">Answer x</button>
+            <button className="btn btn-lg btn-primary text-light rounded my-1">Answer x</button>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
