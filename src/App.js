6ddb7a12ed86c37053b20a383e80d8602d0ba412
Antrâxx <h.julien62@gmail.com>
2022-06-12 00:15:09 +0200

    Api chargée dans le DOM

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from "react";
 import Tableau from "./Tableau";
+import axios from "axios";
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      quizzs: [
-        {id: 0, step: 1, question: "todo q1", guesses: [1, 2, 3, 4], answer: "todo rep1"},
-        {id: 1, step: 2, question: "todo q2", guesses: [1, 2, 3, 4], answer: "todo rep2"},
-        {id: 2, step: 3, question: "todo q3", guesses: [1, 2, 3, 4], answer: "todo rep3"},
-      ]
+      quizzs: []
     }
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:3001/quizzs").then(response => this.setState({ quizzs: response.data }));
   }
 
   render() {
     return (
-      <div>
-        <div id="panel">
-          <span id="step">{this.state.quizzs[0].step}</span>
-        </div>
+      <div className="card">
+
         <div className="container d-flex justify-content-center align-content-center">
   
-          <Tableau quizz={this.state.quizzs} />
+          {this.state.quizzs.map(quizz => <Tableau quizz={quizz} key={quizz.id}/>)}
   
         </div>
       </div>
--- src/Tableau.js
+++ src/Tableau.js
@@ -3,19 +3,21 @@ import React from "react";
 class Tableau extends React.Component {
     render() {
         return (
-            <div className="col-12 d-flex flex-column">
+
+
+            <div className="col-12 d-flex flex-column justify-content-center align-items-center">
+                <div id="panel">
+                    <span id="step">Question {this.props.quizz.step} sur 3</span>
+                </div>
                 <div className="panelQuestion d-flex flex-column align-items-center">
                 <h1 id="title">My Quizz</h1>
-                <h2 id="question">{this.props.quizz[0].question}</h2>
-                </div>
+                <h2 id="question">{this.props.quizz.question}</h2>
+            </div>
     
-                <div className="panelAnswer d-flex flex-column">
-                    {this.props.quizz[0].guesses.map(guess =>
-                        <button className="btn btn-lg btn-primary text-light rounded my-1">{guess}</button>
-                    )}
-                {/* <button className="btn btn-lg btn-primary text-light rounded my-1">{this.props.quizz[0].guess[1]}</button>
-                <button className="btn btn-lg btn-primary text-light rounded my-1">{this.props.quizz[0].guess[2]}</button>
-                <button className="btn btn-lg btn-primary text-light rounded my-1">{this.props.quizz[0].guess[3]}</button> */}
+            <div className="col-6 panelAnswer d-flex flex-column">
+                {this.props.quizz.guesses.map(guess => 
+                     <button className="btn btn-lg btn-primary text-light rounded my-1 rounded">{guess}</button> 
+                )}
                 </div>
             </div>
 
